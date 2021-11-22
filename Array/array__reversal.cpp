ea7d05f0dcea7620b0a91e920941d53b1ff04c73
Deepanshu Dahiya <66161514+Deepanshu-dahiya@users.noreply.github.com>
2021-11-22 10:31:40 +0530

    Add files via upload

---
+++ Array/array__reversal.cpp
@@ -0,0 +1,49 @@
+/* Write a program for array or string reversal */
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){cin>>arr[i];}
+    cout<<"input array is  ";
+    for(int i=0;i<n;i++){cout<<arr[i]<<" ";}
+    int start=0,end=n-1;
+    while(start<end){
+        int temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+        start+=1;
+        end-=1;
+    }
+    cout<<endl<<"reversed array is ";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
