7e571b4df81e853b4c1dd8c2a2d84ed7c4e82d3c
Deepanshu Dahiya <66161514+Deepanshu-dahiya@users.noreply.github.com>
2022-03-20 15:20:59 +0530

    Permutation

---
+++ Dynamic Programming/Permutation.cpp
@@ -0,0 +1,47 @@
+/*Permutation coefficient Problem*/
+// A O(n) solution that uses
+// table fact[] to calculate
+// the Permutation Coefficient
+#include<bits/stdc++.h>
+using namespace std;
+
+// Returns value of Permutation
+// Coefficient P(n, k)
+int permutationCoeff(int n, int k)
+{
+	int fact[n + 1];
+
+	// Base case
+	fact[0] = 1;
+
+	// Calculate value
+	// factorials up to n
+	for(int i = 1; i <= n; i++)
+	fact[i] = i * fact[i - 1];
+
+	// P(n,k) = n! / (n - k)!
+	return fact[n] / fact[n - k];
+}
+int PermutationCoeff_withZero_Extra_Space(int n, int k)
+{
+    int P = 1;
+
+    // Compute n*(n-1)*(n-2)....(n-k+1)
+    for (int i = 0; i < k; i++)
+        P *= (n-i) ;
+
+    return P;
+}
+// Driver Code
+int main()
+{
+	int n = 10, k = 2;
+
+	cout << "Value of P(" << n << ", "
+		<< k << ") is "
+		<< permutationCoeff(n, k);
+
+	return 0;
+}
+
+// This code is contributed by shubhamsingh10
